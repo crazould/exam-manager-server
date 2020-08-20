764fae62b1b4a3cf5f51672628075a262abe6402
crazould <crazould@gmail.com>
2020-08-21 00:38:07 +0700

    change status column table

--- database/migrations/2020_08_03_041413_create_test_headers_table.php
+++ database/migrations/2020_08_03_041413_create_test_headers_table.php
@@ -16,7 +16,6 @@ class CreateTestHeadersTable extends Migration
         Schema::create('test_headers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('status');
             $table->timestamps();
         });
     }
